0024a16e98ad75ed11ffeacd856d3a75c13fa087
Dhriti Saluja <dhritisaluja1999@gmail.com>
2025-02-13 15:27:46 +0530

    Updated server.js

--- demo-projects/3-dockerfile/app/server.js
+++ demo-projects/3-dockerfile/app/server.js
@@ -26,51 +26,26 @@ app.get('/profile-picture', function (req, res) {
 });
 
 // Database Connection depending on where your node js app is running, it chooses different MongoDB addresses.
+
 // use this MongoDB address when starting your application locally with node command
 let mongoUrlLocal = "mongodb://admin:pass@localhost:27017";
 
-// use when starting your application as a separate docker container
-let mongoUrlDocker = "mongodb://admin:pass@host.docker.internal:27017";
-
-// use when starting your application as docker container, part of docker-compose
+// use when starting application as docker container, part of docker-compose
 let mongoUrlDockerCompose = "mongodb://admin:pass@mongodb";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
-// "my-db" as db name in demo with docker.
+// "my-db" in demo with docker
 let databaseName = "my-db";
-
-//When a user submits updated profile information, the server saves it into the database
-// and then sends that same information back as a confirmation.
-app.post('/update-profile', function (req, res) {
-  let userObj = req.body;
-
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
-
-    let db = client.db(databaseName);
-    userObj['userid'] = 1;
-
-    let myquery = { userid: 1 };
-    let newvalues = { $set: userObj };
-
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
-      if (err) throw err;
-      client.close();
-    });
-
-  });
-  // Send response
-  res.send(userObj);
-});
-
+// "users" is the table name inside "my-db" database.
+let collectionName = "users";
 
 // When you visit /get-profile, the server looks up the user with userid 1
 // in the database and sends you the stored profile information.
 app.get('/get-profile', function (req, res) {
   let response = {};
-  // Connect to the db
+  // Connect to the db using local application or docker compose variable in connection properties
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
@@ -78,7 +53,7 @@ app.get('/get-profile', function (req, res) {
 
     let myquery = { userid: 1 };
 
-    db.collection("users").findOne(myquery, function (err, result) {
+    db.collection(collectionName).findOne(myquery, function (err, result) {
       if (err) throw err;
       response = result;
       client.close();
@@ -89,8 +64,31 @@ app.get('/get-profile', function (req, res) {
   });
 });
 
+//When a user submits updated profile information, the server saves it into the database
+// and then sends that same information back as a confirmation.
+app.post('/update-profile', function (req, res) {
+  let userObj = req.body;
+  // Connect to the db using local application or docker compose variable in connection properties
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+    if (err) throw err;
+
+    let db = client.db(databaseName);
+    userObj['userid'] = 1;
+
+    let myquery = { userid: 1 };
+    let newvalues = { $set: userObj };
+
+    db.collection(collectionName).updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+      if (err) throw err;
+      client.close();
+    });
+
+  });
+  // Send response
+  res.send(userObj);
+});
+
 // tells the server to listen for any incoming requests on port 3000
 app.listen(3000, function () {
   console.log("app listening on port 3000!");
 });
-
